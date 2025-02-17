cb3139040ba09781f57cee92f487a247b831d5e1
schmeddy99 <ahmedaosman00@gmail.com>
2025-02-17 10:40:12 -0800

    initial commit

---
+++ eagles_game.py
@@ -0,0 +1,26 @@
+print(r'''
+*******************************************************************************
+///,        ////
+\ `/,      /` .&gt;.
+ \  /,   _/` /.
+  \_ `/_/`  /.
+    \__/_  .&lt;
+    /&lt;&lt;&lt;&lt;\_\_
+   /,)^&gt;&gt;_._\\
+   (/   \\ /\\\
+        // ````
+     ,-((--,/|
+     &gt;_&gt;-~~``'
+*******************************************************************************
+''')
+print("Welcome to Eagles Island.")
+print("Your mission is to find the Lombardi Trophy.")
+play = input("Would you like to play? Y/N\n")
+if play == 'Y' or 'y':
+    print("You encounter a man packing cheese.")
+    fight_packers = input("Do you choose to fight? Y/N\n")
+
+else:
+    print("Lame. Hope you play again another time.")
+
+
